cc451d2f484257b2f71863b3f7d16c2b2d00db5f
Cenk Ozkan <cenk.ozkan@agilatek.com>
2022-12-19 23:01:43 +0300

    added card actions

--- src/app/shared/header/header-middle/header-middle.component.ts
+++ src/app/shared/header/header-middle/header-middle.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { CardState } from '../../../state/card.state';
+import { GetCards } from '../../../action/card.action';
 
 @Component({
   selector: 'app-header-middle',
   templateUrl: './header-middle.component.html',
-  styleUrls: ['./header-middle.component.scss']
+  styleUrls: ['./header-middle.component.scss'],
 })
 export class HeaderMiddleComponent implements OnInit {
+  constructor(private store: Store) {}
 
-  constructor() { }
+  @Select(CardState.getCardList) cards$: Observable<any> | undefined;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    return this.store.dispatch(new GetCards());
   }
-
 }
