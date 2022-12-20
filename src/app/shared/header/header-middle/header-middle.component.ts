3e389ff03239834b53975247af46b8efdcdc0215
Cenk Ozkan <cenk.ozkan@agilatek.com>
2022-12-20 23:08:29 +0300

    added card actions

--- src/app/shared/header/header-middle/header-middle.component.ts
+++ src/app/shared/header/header-middle/header-middle.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
-import { CardState } from '../../../state/card.state';
-import { GetCards } from '../../../action/card.action';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {Select, Store} from '@ngxs/store';
+import {Observable} from 'rxjs';
+import {CardState} from '../../../state/card.state';
+import {GetCards} from '../../../action/card.action';
 
 @Component({
   selector: 'app-header-middle',
@@ -12,6 +12,7 @@ import { GetCards } from '../../../action/card.action';
 })
 export class HeaderMiddleComponent implements OnInit {
   totalPrice?: any = 0;
+
   constructor(private store: Store) {}
 
   @Select(CardState.getCardList) cards$: Observable<any> | undefined;
@@ -19,12 +20,14 @@ export class HeaderMiddleComponent implements OnInit {
   ngOnInit() {
     this.getCard();
     this.cards$?.subscribe((val) => {
+      let priceArray: any[] = [];
       val.forEach((value: any, key: any) => {
-        console.log(value.price, value, key);
-        this.totalPrice += value.price;
+        return priceArray.push(value.price);
       });
-      console.log(parseFloat(String(this.totalPrice)).toFixed());
-      return parseFloat(String(this.totalPrice)).toFixed(2);
+
+      this.totalPrice = priceArray.reduce((acc, val) => {
+        return parseFloat((acc + val).toFixed(2));
+      }, 0);
     });
   }
 
